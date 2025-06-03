82af6a01119937272f3d369c65a74db5f787e2fa
LiamJohns96 <89575079+LiamJohns96@users.noreply.github.com>
2025-06-03 15:56:39 +1000

    Initial Commit

---
+++ BlockNotes/BlockNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockNotesApp.swift
+//  BlockNotes
+//
+//  Created by Liam on 3/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlockNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
